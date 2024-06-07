069ef02bac8e0686580535b99e2908978c15c3b6
mustafamadjid <madjidmustafa@gmail.com>
2024-06-07 08:42:31 +0700

    Update Javascript

---
+++ JS-26-Do While Loop/DoWhile.js
@@ -0,0 +1,10 @@
+// cek kondisi setelah dijalankan satu kali
+// Dijalankan minimal satu kali walaupun kondisi tidak memenuhi syarat perulangan nantinya
+
+let counter = 100;
+
+do {
+  document.writeln(`Perulangan ke ${counter}`);
+  document.writeln("<br>");
+  counter++;
+} while (counter <= 10);
