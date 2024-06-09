05d94a75923da80c59d69cc3ff1133534c850ff2
mustafamadjid <madjidmustafa@gmail.com>
2024-06-09 11:07:55 +0700

    Function

--- JS-31-With/With.js
+++ JS-31-With/With.js
@@ -4,4 +4,19 @@
 
   dengan menggunakan with kita bisa mengakses property dalam sebuah data tanpa harus menyebut datanya
 
+  Sudah jarang digunakan karena kadang membuat ambigu ketka terdapat nama property yang sama 
+  dengan property lain atau variabel lain
+
 */
+
+const person = {
+  firstName: "Atha",
+  middelName: "Mustafa",
+  lastName: "Madjid",
+};
+
+with (person) {
+  document.writeln(firstName);
+  document.writeln(middelName);
+  document.writeln(lastName);
+}
---
+++ JS-32-Function/func.js
@@ -0,0 +1,35 @@
+// Create a function without parameter
+function sayHello() {
+  document.writeln("Hello World");
+}
+
+// Create a function with parameter
+function sayName(firstName, lastName) {
+  document.writeln(`Hello ${firstName} ${lastName}`);
+}
+
+// Create a function with parameter and return value
+function sayCity(city) {
+  const say = `Welcome to ${city}`;
+  return say;
+}
+
+/* 
+
+  Dalam javascript secara default, parameter dalam function adalah optional
+
+*/
+
+// Function with optional parameter
+function sayIt(firstName, middleName, lastName) {
+  document.writeln(`${firstName} ${middleName} ${lastName}`);
+}
+
+// Call the function
+sayHello();
+document.writeln("</br>");
+sayName("Mustafa", "Madjid");
+document.writeln("</br>");
+document.writeln(sayCity("Jakarta"));
+document.writeln("</br>");
+sayIt("Nuril");
