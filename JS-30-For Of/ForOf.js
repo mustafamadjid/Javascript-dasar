2b1d6661b4bf40780cecf22692c3d84bc80773e9
mustafamadjid <madjidmustafa@gmail.com>
2024-06-09 10:46:36 +0700

    For of

--- JS-30-For Of/ForOf.js
+++ JS-30-For Of/ForOf.js
@@ -1,1 +1,14 @@
 //  Iterasi terhadap value
+let cities = ["Jakarta", "Samarinda", "Tanjung Pinang"];
+
+for (const i of cities) {
+  document.writeln(`City name : ${i}`);
+  document.writeln("</br>");
+}
+
+document.writeln("</br>");
+const name = "Harimurti";
+for (const i of name) {
+  document.writeln(`Char of Harimurti : ${i}`);
+  document.writeln("</br>");
+}
---
+++ JS-31-With/With.js
@@ -0,0 +1,7 @@
+/* 
+
+  with -> fitur untuk menurunkan sebuah scope data
+
+  dengan menggunakan with kita bisa mengakses property dalam sebuah data tanpa harus menyebut datanya
+
+*/
