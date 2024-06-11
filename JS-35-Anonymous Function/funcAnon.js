81f425d808579c9ae41704537c7263ce35cd81af
mustafamadjid <madjidmustafa@gmail.com>
2024-06-11 17:46:41 +0700

    Staged

--- JS-35-Anonymous Function/funcAnon.js
+++ JS-35-Anonymous Function/funcAnon.js
@@ -5,3 +5,23 @@
   dapat dibuat dalam sebuah variable atau saat mengisi parameter
 
 */
+
+// Di dalam variabel
+let say = function (name) {
+  document.writeln(`Hello ${name}`);
+};
+
+say("Fuhrer !!");
+document.writeln("</br>");
+document.writeln("</br>");
+
+// Di dalam parameter function
+function giveName(callback) {
+  callback("Muliadi");
+}
+
+giveName(say);
+
+giveName(function (name) {
+  document.writeln(`Hallo abang ${name}`);
+});
